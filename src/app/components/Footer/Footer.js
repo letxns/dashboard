a26b3630f1a5d961931e055ba591d22555be0348
Leticia Martins <smartins.leticia@hotmail.com>
2024-08-13 01:36:25 -0300

    feat: add footer

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+  return (
+    <footer className="text-center"> Developed by <a href="https://github.com/letxns" target="_blank">Leticia</a> &copy;</footer>
+  )
+}
