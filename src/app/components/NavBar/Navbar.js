e8c4a03f3858ee489518742e95e2b7365e94bde3
Leticia Martins <smartins.leticia@hotmail.com>
2024-08-13 01:29:02 -0300

    navbar adjustment - add export button

--- src/app/components/NavBar/Navbar.js
+++ src/app/components/NavBar/Navbar.js
@@ -1,11 +1,15 @@
+import ExcelExportButton from "../ExportButton/ExportButton";
+import Sidebar from "../SideBar/SideBar";
+
 export default function Navbar() {
   return (
-    <nav className="flex-row items-center flex-wrap p-2 w-full bg-tradesee-blue">
+    <nav className="fixed flex-row items-center flex-wrap p-2 w-full bg-tradesee-blue">
       <div className="flex items-center flex-shrink-0 text-white justify-between">
-        <span className="font-semibold text-xl tracking-tight">Dashboard</span>
-        <a href="" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white lg:mt-0">
-          Exportar dados
-        </a>
+        <div className="flex flex-row place-items-center">
+          <Sidebar />
+          <h1 className="font-semibold text-xl tracking-tight">Dashboard</h1>
+        </div>
+        <ExcelExportButton />
       </div>
     </nav>
   )
